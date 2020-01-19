b105b3bb3ce7635cb2a6b5e4463197049596d409
saranjothi <54699601+saranjothi@users.noreply.github.com>
2020-01-19 20:22:23 +0530

    Update devops.java

--- devops-training2/src/devops.java
+++ devops-training2/src/devops.java
@@ -1,6 +1,8 @@
 
 public class devops {
 	public static void main(String args[]){  
-	     System.out.println("Hello Java");  
+	     System.out.println("Hello Java");
+		System.out.println("hi");
+		System.out.println("Hello");
 	    }  
 }
